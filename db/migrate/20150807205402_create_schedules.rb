c62390f9c25d22968b7424b4218d6439f1dd8512
violadolayinka <violadolayinka@gmail.com>
2015-08-11 10:05:27 -0700

    Returning to work on app. Hope to be completed by EOD.

--- db/migrate/20150807205402_create_schedules.rb
+++ db/migrate/20150807205402_create_schedules.rb
@@ -1,10 +1,12 @@
+##Create a Schedules table on database 
 class CreateSchedules < ActiveRecord::Migration
   def change
     create_table :schedules do |t|
+##This specifies the fields within the database and the type as a string. 
       t.string :name
       t.string :month
       t.string :day
-
+##also add timestmps by default (this is automatically translated to created_at and updated_at datetime fields)
       t.timestamps null: false
     end
   end
